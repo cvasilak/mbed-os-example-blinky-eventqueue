9dd9c7f29cbf912b0e538601f4e1bbdbe0e1b189
Christos Vasilakis <christos.vasilakis@arm.com>
2019-11-29 14:21:30 +0000

    first attempt with eventqueue

--- main.cpp
+++ main.cpp
@@ -15,18 +15,30 @@
  */
 
 #include "mbed.h"
-#include "platform/mbed_thread.h"
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE_MS                                                    500
+DigitalOut led1(LED1);
+InterruptIn sw(SW2);
+EventQueue queue(32 * EVENTS_EVENT_SIZE);
+Thread t;
 
-int main()
-{
-    // Initialise the digital pin LED1 as an output
-    DigitalOut led(LED1);
+void rise_handler(void) {
+    printf("rise_handler in context %p\r\n", Thread::gettid());
+    // Toggle LED
+    led1 = !led1;
+}
 
-    while (true) {
-        led = !led;
-        thread_sleep_for(BLINKING_RATE_MS);
-    }
+void fall_handler(void) {
+    printf("fall_handler in context %p\r\n", Thread::gettid());
+    // Toggle LED
+    led1 = !led1;
+}
+
+int main() {
+    // Start the event queue
+    t.start(callback(&queue, &EventQueue::dispatch_forever));
+    printf("Starting in context %p\r\n", Thread::gettid());
+    // The 'rise' handler will execute in IRQ context
+    sw.rise(rise_handler);
+    // The 'fall' handler will execute in the context of thread 't'
+    sw.fall(queue.event(fall_handler));
 }
