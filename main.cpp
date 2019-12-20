64c7a5cae8cc094002b85bb509dc2a41058f8f52
Christos Vasilakis <christos.vasilakis@arm.com>
2019-12-20 20:08:56 +0000

    refactor to call in interrupt context and split the non-critical path

--- main.cpp
+++ main.cpp
@@ -27,6 +27,12 @@ Serial pc(USBTX, USBRX);
 
 void rise_handler_user_context(void) {
     pc.printf("rise_handler_user_context in context %p\r\n", osThreadGetId());
+    pc.printf("Blink! LED1 is now %d\r\n", led1.read());
+}
+
+void fall_handler_user_context(void) {
+    pc.printf("fall_handler_user_context in context %p\r\n", osThreadGetId());
+    pc.printf("Blink! LED1 is now %d\r\n", led1.read());
 }
 
 void rise_handler(void) {
@@ -38,20 +44,19 @@ void rise_handler(void) {
 }
 
 void fall_handler(void) {
-    pc.printf("fall_handler in context %p\r\n", osThreadGetId());
-    
-    // Toggle LED
+    // Execute the time critical part first
     led1 = !led1;
-    
-    pc.printf("Blink! LED1 is now %d\r\n", led1.read());
+    // The rest can execute later in user context (and can contain code that's not interrupt safe).
+    // We use the 'queue.call' function to add an event (the call to 'fall_handler_user_context') to the queue.
+    queue.call(fall_handler_user_context);
 }
 
 int main() {
     // Start the event queue
     t.start(callback(&queue, &EventQueue::dispatch_forever));
     pc.printf("Starting in context %p\r\n", osThreadGetId());
-    // The 'rise' handler will execute in the context of thread 't'
-    sw.rise(queue.event(rise_handler));
-    // The 'fall' handler will execute in the context of thread 't'
-    sw.fall(queue.event(fall_handler));
+    // The 'rise' handler will execute in interrupt context
+    sw.rise(rise_handler);
+    // The 'fall' handler will execute in interrupt context
+    sw.fall(fall_handler);
 }
