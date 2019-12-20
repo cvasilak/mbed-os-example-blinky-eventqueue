a95234d96e7636d9b60d0cb9b5864fb13ba51f9f
Christos Vasilakis <christos.vasilakis@arm.com>
2019-12-20 17:33:07 +0000

    replace deprecated methods and add serial output support.

--- main.cpp
+++ main.cpp
@@ -15,30 +15,43 @@
  */
 
 #include "mbed.h"
+#include "mbed_events.h"
 
 DigitalOut led1(LED1);
-InterruptIn sw(SW2);
+InterruptIn sw(USER_BUTTON);
 EventQueue queue(32 * EVENTS_EVENT_SIZE);
 Thread t;
 
+// define the Serial object
+Serial pc(USBTX, USBRX);
+
+void rise_handler_user_context(void) {
+    pc.printf("rise_handler_user_context in context %p\r\n", osThreadGetId());
+}
+
 void rise_handler(void) {
-    printf("rise_handler in context %p\r\n", Thread::gettid());
-    // Toggle LED
+    // Execute the time critical part first
     led1 = !led1;
+    // The rest can execute later in user context (and can contain code that's not interrupt safe).
+    // We use the 'queue.call' function to add an event (the call to 'rise_handler_user_context') to the queue.
+    queue.call(rise_handler_user_context);
 }
 
 void fall_handler(void) {
-    printf("fall_handler in context %p\r\n", Thread::gettid());
+    pc.printf("fall_handler in context %p\r\n", osThreadGetId());
+    
     // Toggle LED
     led1 = !led1;
+    
+    pc.printf("Blink! LED1 is now %d\r\n", led1.read());
 }
 
 int main() {
     // Start the event queue
     t.start(callback(&queue, &EventQueue::dispatch_forever));
-    printf("Starting in context %p\r\n", Thread::gettid());
-    // The 'rise' handler will execute in IRQ context
-    sw.rise(rise_handler);
+    pc.printf("Starting in context %p\r\n", osThreadGetId());
+    // The 'rise' handler will execute in the context of thread 't'
+    sw.rise(queue.event(rise_handler));
     // The 'fall' handler will execute in the context of thread 't'
     sw.fall(queue.event(fall_handler));
 }
